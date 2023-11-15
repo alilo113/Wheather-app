e73d023bb13d3a6ca6a14d2d677ab38d00e47fe1
unknown <aliopdandan@gmail.com>
2023-11-15 21:08:58 +0100

    commit

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,50 +1,49 @@
-import "./index.css"
+import "./index.css";
 import { useState } from "react";
 
 function App() {
-  const data = {
-    temperature: 25,
-    description: 'Sunny',
-    city: 'New York',
-    country: 'US'
-  };
-
   const [city, setCity] = useState("");
-  const [weatherData, setWeatherData] = useState()
+  const [weatherData, setWeatherData] = useState(null);
 
-  async function fetchWheaderData(e){
-    e.preventDefault(); // Prevent default form submission
-    setCity("")
-    const key = "e547bf9a8904d13ac7b15b4de27445cf"
+  async function fetchWeatherData(e) {
+    e.preventDefault();
+    setCity("");
+    const key = import.meta.env.VITE_REACT_APP_API_KEY;
     try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
-      const res = await fetch(url)
-      const data = await res.json()
-      console.log(data)
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data.name);
+      console.log(data.sys.country);
+      setWeatherData(data);
     } catch (error) {
-      console.log(error) 
+      console.log(error);
     }
   }
 
   return (
     <div className="weather">
       <h2>Current Weather</h2>
-      <form onSubmit={fetchWheaderData}>
+      <form onSubmit={fetchWeatherData}>
         <input
           type="text"
           placeholder="Enter city"
           value={city}
-          onChange={(e) => {setCity(e.target.value)}}
+          onChange={(e) => setCity(e.target.value)}
         />
         <button type="submit">Get Weather</button>
       </form>
       <div>
-        <p>Location: {data.city}, {data.country}</p>
-        <p>Temperature: {data.temperature}°C</p>
-        <p>Description: {data.description}</p>
+        {weatherData && (
+          <div>
+            <p>Location: {weatherData.name}, {weatherData.sys.country}</p>
+            <p>Temperature: {weatherData.main.temp}°C</p>
+            <p>Description: {weatherData.weather[0].description}</p>
+          </div>
+        )}
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
